669585d86d2dbd337546473b70044e2c9bc2debc
Idy <puiwalam09@gmail.com>
2022-11-29 18:20:09 +0800

    Add find by date api. Update put/patch api

--- src/main/java/com/deepbluestudio/todobackend/controllers/TodoController.java
+++ src/main/java/com/deepbluestudio/todobackend/controllers/TodoController.java
@@ -4,6 +4,8 @@ import com.deepbluestudio.todobackend.models.Todo;
 import com.deepbluestudio.todobackend.payload.response.EStatus;
 import com.deepbluestudio.todobackend.payload.response.ResponseHandler;
 import com.deepbluestudio.todobackend.repository.TodoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.OffsetDateTime;
+import java.util.Date;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -20,16 +24,24 @@ import java.util.Optional;
 public class TodoController {
     final TodoRepository todoRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(TodoController.class);
+
     @Autowired
     public TodoController(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTodoById(@PathVariable("id") Long id) {
+        Optional<Todo> todo = todoRepository.findById(id);
+        return ResponseHandler.generateResponse(EStatus.SUCCESS.getStatus(), HttpStatus.OK, todo);
+    }
+
     @GetMapping
-    public ResponseEntity<?> get(@RequestParam(value = "page", defaultValue = "1") Integer page,
-                                 @RequestParam(value = "size", defaultValue = "20") Integer size,
-                                 @RequestParam(value = "sort", defaultValue = "updatedAt") String sort,
-                                 @RequestParam(value = "order", defaultValue = "desc") String order) {
+    public ResponseEntity<?> getTodos(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                      @RequestParam(value = "size", defaultValue = "20") Integer size,
+                                      @RequestParam(value = "sort", defaultValue = "updatedAt") String sort,
+                                      @RequestParam(value = "order", defaultValue = "desc") String order) {
         Page<Todo> todos = todoRepository.findAll(PageRequest.of(
                 page - 1,
                 size,
@@ -40,14 +52,52 @@ public class TodoController {
                 todos.getContent(), todos.getPageable(), todos.getTotalElements());
     }
 
+    @GetMapping("/findAllByDateBetween")
+    public ResponseEntity<?> findAllByDateBetween(@RequestParam(value = "startDate") String startDate,
+                                                  @RequestParam(value = "endDate") String endDate,
+                                                  @RequestParam(value = "page", defaultValue = "1") Integer page,
+                                                  @RequestParam(value = "size", defaultValue = "20") Integer size,
+                                                  @RequestParam(value = "sort", defaultValue = "updatedAt") String sort,
+                                                  @RequestParam(value = "order", defaultValue = "desc") String order) {
+
+        OffsetDateTime parsedStartDateTime = OffsetDateTime.parse(startDate);
+        OffsetDateTime parsedEndDate = OffsetDateTime.parse(endDate);
+
+        Date startDateTime = Date.from(parsedStartDateTime.toInstant());
+        Date endDateTime = Date.from(parsedEndDate.toInstant());
+
+        logger.error(startDateTime + ", " + endDateTime);
+
+        Page<Todo> todos = todoRepository.findAllByDateBetween(
+                startDateTime,
+                endDateTime,
+                PageRequest.of(
+                        page - 1,
+                        size,
+                        order.equals("desc")
+                                ? Sort.by(sort).descending()
+                                : Sort.by(sort).ascending()));
+        return ResponseHandler.generateResponseWithPaging(EStatus.SUCCESS.getStatus(), HttpStatus.OK, todos.getContent(), todos.getPageable(), todos.getTotalElements());
+    }
+
     @PostMapping
-    public ResponseEntity<?> add(@RequestBody Todo todo) {
+    public ResponseEntity<?> addTodo(@RequestBody Todo todo) {
         Object saveResult = todoRepository.save(todo);
         return ResponseHandler.generateResponse(EStatus.SUCCESS.getStatus(), HttpStatus.OK, saveResult);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
+        Object updateResult = null;
+        Optional<Todo> existingTodo = todoRepository.findById(id);
+        if (existingTodo.isPresent()) {
+            updateResult = todoRepository.save(todo);
+        }
+        return ResponseHandler.generateResponse(EStatus.SUCCESS.getStatus(), HttpStatus.OK, updateResult);
+    }
+
     @PatchMapping("/{id}")
-    public ResponseEntity<?> patch(@PathVariable("id") Long id, @RequestBody Todo todo) {
+    public ResponseEntity<?> patchTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
         Object updateResult = null;
         Optional<Todo> existingTodo = todoRepository.findById(id);
         if (existingTodo.isPresent()) {
@@ -59,7 +109,7 @@ public class TodoController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+    public ResponseEntity<?> deleteTodo(@PathVariable("id") Long id) {
         todoRepository.deleteById(id);
         return ResponseHandler.generateResponseWithoutData(EStatus.SUCCESS.getStatus(), HttpStatus.OK);
     }
--- src/main/java/com/deepbluestudio/todobackend/repository/TodoRepository.java
+++ src/main/java/com/deepbluestudio/todobackend/repository/TodoRepository.java
@@ -1,9 +1,14 @@
 package com.deepbluestudio.todobackend.repository;
 
 import com.deepbluestudio.todobackend.models.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    Page<Todo> findAllByDateBetween(Date startDate, Date endDate, Pageable pageable);
 }
